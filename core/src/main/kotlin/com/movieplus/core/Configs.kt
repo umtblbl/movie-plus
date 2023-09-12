4e5b3336e2f9eddb0c200ef4174bbc41355d2471
Github <r.umit.blbl@gmail.com>
2023-09-12 17:49:14 +0300

    MP-004 - Create Movie Detail Screen

--- core/src/main/kotlin/com/movieplus/core/Configs.kt
+++ core/src/main/kotlin/com/movieplus/core/Configs.kt
@@ -5,7 +5,7 @@ object Configs {
         const val BASE_URL: String = BuildConfig.MOVIE_PLUS_API_BASE_URL
         const val CACHE_SIZE: Long = 10 * 1024 * 1024 // 10 MiB
         const val TIME_OUT: Long = 3 * 60
-        const val TOKEN: String = ""
+        const val TOKEN: String = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYThmNzRmY2M4ZDgwZjFhMTY5NDA0YzQwNjA5NzFjOCIsInN1YiI6IjY0ZWRkNzYxYzYxM2NlMDEyY2M3MDEwYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LMw5WfpT4ILsRd3NLWVAr167PnCzFpmgoXyeWhzpAeA"
         const val IMAGE_URL = "https://image.tmdb.org/t/p/w500/"
     }
 
